741faea4a2317902bb175395419aa71de2dc4ae8
Alexey Polkanov <apolkanov@yandex-team.ru>
2013-06-11 22:30:36 +0700

    Finds faster than 330 in average

--- main.cpp
+++ main.cpp
@@ -6,16 +6,20 @@
 
 using namespace std;
 typedef long long int64;
+typedef vector<pair<double, int64> > Digits;
 
 class Guesser {
 
 public:
-  Guesser(int64 N, int64 fraud) : mN(N), mFraud(fraud), mAttempt(0) {
+  Guesser(int64 N, int64 fraud) : mN(N), mFraud(fraud), mAttempt(0), MULT(4) {
     mL.resize(mN);
+	for (size_t i = 0; i < mL.size(); ++i) {
+		mL[i].first = 1;
+	}
   }
 
   int64 Next() {
-    return next_number(mL, mFraud);
+    return next_number1(mL, mFraud);
   }
 
   void Answer(int64 num, bool answer) {
@@ -31,34 +35,42 @@ public:
   int64 GetAttempt() { return mAttempt; }
 
 private:
-int64 next_number(const vector<pair<int64, int64> >& l, int64 fraud_number) {
- double sum = 0;
+
+int64 next_number1(Digits& l, int64 fraud_number) {
+ double sumLeft = 0, sumRight = 0;
  double m = 0;
  int64 i = 0;
- vector<pair<int64, int64> >::const_iterator it = l.begin();
- for (; it != l.end(); ++it) {
-  if (it->second <= fraud_number) { 
-    double w = pow(1.2, fraud_number - it->second + 1.0);
-    sum += w;
-    m += w * i;
-  }
-  ++i;  
+ 
+ int64 left = 0;
+ int64 right = l.size() - 1;
+ while(left < right) {
+	 
+	 while(sumLeft <= sumRight && left < right) {
+		sumLeft += l[left++].first;
+	 }
+	 if (left < right)
+		 sumRight += l[right--].first;
  }
- if (0 == sum)
-  return l.size() / 2;
- return m / sum;
+ int64 res = right > l.size() / 2 + 1 ? right  : right - 1;
+ return res;
 }
 
-void apply_answer(vector<pair<int64, int64> >& l, int64 number, bool answer) {	
+void apply_answer(Digits& l, int64 number, bool answer) {	
 	for(int64 i = 0; i < l.size(); ++i) {
-		if ((i <= number && answer) || (i > number && !answer))
+		if (l[i].second > mFraud) {
+			l[i].first = 0;
+			continue;
+		}
+		if ((i <= number && answer) || (i > number && !answer)) {
 			l[i].second++;
-	    else
-			l[i].first++;
+			l[i].first /= MULT;
+		}
+	    //else
+		//	l[i].first *= MULT;
 	}
 }
 
-int64 answer_found(vector<pair<int64, int64> >& l, int64 number) {
+int64 answer_found(Digits& l, int64 number) {
 	int64 answer = -1;
 	int64 count = 0;
 	for(int64 i = 0; i < l.size(); ++i) {
@@ -75,28 +87,29 @@ int64 answer_found(vector<pair<int64, int64> >& l, int64 number) {
 private:
   int64 mN;
   int64 mFraud;
-  vector<pair<int64, int64> > mL;
+  Digits mL;
   int64 mAttempt;
+  const double MULT;
 };
 
 
 void check() {
 
   Guesser g(1000000, 100);
-  int64 digit = 50011;
-  int i = 0;
+  int64 digit = 453456;
+  int lie = 0;
   while(g.Guess() == -1) {
-    i++;
     int64 number = g.Next();
-		bool answer = digit > number;
-    int lie = 0;
-    //if (lie < 100) {
-  		//bool answer2 = 100000 > number;
-    //  if (answer2 != answer) {
-    //    answer = !answer;
-    //    lie++;
-    //  }
-    //}
+	cout << " " << number << " ";
+	bool answer = digit > number;
+    if (lie < 100) {
+  		bool answer2 = 100000 > number;
+        if (answer2 != answer) {
+          answer = !answer;
+          lie++;
+		  cout << "lie: " << lie << endl;
+       }
+    }
     g.Answer(number, answer);
 	}
   cout << std::endl << "Answer is: " << g.Guess() << std::endl;
@@ -107,6 +120,6 @@ void check() {
 int main() {
   srand(time(0));
   check();
-	system("pause");
+  system("pause");
   return 0;
 }
