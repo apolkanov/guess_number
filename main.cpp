50655a7b98e49421c18d96c843283572d56979fa
Polkanov Alexey <apolkanov@coccoc.vn>
2013-06-11 13:07:42 +0700

    Auto Check function

--- main.cpp
+++ main.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
+typedef long long int64;
 
-int next_number(const vector<pair<int, int> >& l, int fraud_number, int attempt) {
-	int sum = 0;
-	int m = 0;
-	int i = 0;
-	vector<pair<int, int> >::const_iterator it = l.begin();
+class Guesser {
+
+public:
+  Guesser(int64 N, int64 fraud) : mN(N), mFraud(fraud), mAttempt(0) {
+    mL.resize(mN);
+  }
+
+  int64 Next() {
+    return next_number(mL, mFraud);
+  }
+
+  void Answer(int64 num, bool answer) {
+    apply_answer(mL, num, answer);
+    mAttempt++;
+    std::cout << mAttempt << std::endl;
+  }
+
+  int64 Guess() {
+    return answer_found(mL, mFraud);
+  }
+
+  int64 GetAttempt() { return mAttempt; }
+
+private:
+int64 next_number(const vector<pair<int64, int64> >& l, int64 fraud_number) {
+	int64 sum = 0;
+	int64 m = 0;
+	int64 i = 0;
+	vector<pair<int64, int64> >::const_iterator it = l.begin();
 	for (; it != l.end(); ++it) {
 		if (it->second <= fraud_number) {	
-		  int w = fraud_number - it->second + 1; 
+		  int64 w = fraud_number - it->second + 1; 
 		  sum += w;
 		  m += w * i;		  
 		}
@@ -22,19 +49,19 @@ int next_number(const vector<pair<int, int> >& l, int fraud_number, int attempt)
 	return m / sum;
 }
 
-void apply_answer(vector<pair<int, int> >& l, int number, bool answer) {	
-	for(int i = 0; i < l.size(); ++i) {
-		if ((i <= number && answer) || (i >= number && !answer))
+void apply_answer(vector<pair<int64, int64> >& l, int64 number, bool answer) {	
+	for(int64 i = 0; i < l.size(); ++i) {
+		if ((i <= number && answer) || (i > number && !answer))
 			l[i].second++;
 	    else
 			l[i].first++;
 	}
 }
 
-int answer_found(vector<pair<int, int> >& l, int number) {
-	int answer = -1;
-	int count = 0;
-	for(int i = 0; i < l.size(); ++i) {
+int64 answer_found(vector<pair<int64, int64> >& l, int64 number) {
+	int64 answer = -1;
+	int64 count = 0;
+	for(int64 i = 0; i < l.size(); ++i) {
 		if (l[i].second > number)
 			count++;
 		else
@@ -45,28 +72,41 @@ int answer_found(vector<pair<int, int> >& l, int number) {
 	return -1;
 }
 
-int main() {
-	int N = 0; // number of digits
-	int fraud_count = 0;
-	cout << "Enter digit count:" << endl;
-	cin >> N;
-	cout << "Enter fraud count:" << endl;
-	cin >> fraud_count;
+private:
+  int64 mN;
+  int64 mFraud;
+  vector<pair<int64, int64> > mL;
+  int64 mAttempt;
+};
 
-	vector<pair<int, int> > l;
-	l.resize(N);
 
-	int answer = -1;
-	int attempt = 0;
-	while((answer = answer_found(l, fraud_count)) == -1) {
-		attempt++;
-		bool answer = 0;
-		int number = next_number(l, fraud_count, attempt);
-		cout << "Greater than " << number << "?" << std::endl;
-		cin >> answer;
-		apply_answer(l, number, answer);
+void check() {
+
+  Guesser g(1000000, 100);
+  int64 digit = 50011;
+  int i = 0;
+  while(g.Guess() == -1) {
+    i++;
+    int64 number = g.Next();
+		bool answer = digit > number;
+    int lie = 0;
+    //if (lie < 100) {
+  		//bool answer2 = 100000 > number;
+    //  if (answer2 != answer) {
+    //    answer = !answer;
+    //    lie++;
+    //  }
+    //}
+    g.Answer(number, answer);
 	}
-	cout << std::endl << "Answer is: " << answer << std::endl;
-	cout << "Attempts: " << attempt << std::endl;
+  cout << std::endl << "Answer is: " << g.Guess() << std::endl;
+  cout << "Attempts: " << g.GetAttempt() << std::endl;
+}
+
+
+int main() {
+  srand(time(0));
+  check();
 	system("pause");
+  return 0;
 }
